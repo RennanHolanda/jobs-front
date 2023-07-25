1fd0d0357487546eb7f841b00f63ec5f1da9b10b
Bruno Rennan <bruno_rennan@hotmail.com>
2023-07-25 10:54:19 -0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import ErrorPage from './routes/ErrorPage.jsx';
+import Jobs from './routes/Jobs.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path:"/jobs",
+        element: <Jobs />
+      }
+    ]
+    
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
---
+++ src/routes/Jobs.jsx
@@ -0,0 +1,11 @@
+import styles from "./Jobs.module.css"
+
+const Jobs = () => {
+  return (
+    <div className={styles.jobs}>
+        <h1>Vagas</h1>
+    </div>
+  )
+}
+
+export default Jobs
